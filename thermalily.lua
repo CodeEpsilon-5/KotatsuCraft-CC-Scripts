f5400af483b4e051f8fd695557f44b1b2ee76dfd
Lucas Daniel Pereira Rocha <lucasdpr1@gmail.com>
2024-01-10 20:10:38 -0300

    remove unused function and workaround solution

--- thermalily.lua
+++ thermalily.lua
@@ -65,40 +65,6 @@ local function handleTerminate()
     return
 end
 
-local function handleEvents()
-    while true do
-        local event = os.pullEvent()
-        local event_type = event[0] or event
-
-        if event_type == "redstone" then
-            print("Redstone signal changed.")
-            local thermalilySignal = redstone.getAnalogInput("right")
-            print("Thermalily signal: ", thermalilySignal)
-            if thermalilySignal == 0 then
-                print("Thermalily Active.")
-                closeValve()
-            else
-                if current_timer ~= 0 then
-                    os.cancelTimer(current_timer)
-                end
-                local delay = (20 * thermalilySignal) + 5
-                print("Waiting", delay.."s", "cooldown...")
-                current_timer = os.startTimer(delay)
-                os.queueEvent("cooldown", delay, os.clock())
-            end
-        elseif event_type == "timer" then
-            local event_id = event[1]
-            print("Cooldown over, opening valve...")
-            openValve()
-            current_timer = 0
-        elseif event_type == "terminate" then
-            closeValve()
-            os.pullEvent = pullEvent
-            os.queueEvent("terminate")
-        end
-    end
-end
-
 local function cooldownCountdown()
     while true do
         local event, delay, start_time = os.pullEvent("cooldown")
@@ -110,7 +76,7 @@ local function cooldownCountdown()
             time_remaining = math.floor(delay - time_elapsed)
             local time_remaining_str = time_remaining .. "s"
             term.setTextColor(colors.blue)
-            term.write("Thermalily Cooldown: "..time_remaining_str.." (Press any key to update)")
+            term.write("Thermalily Cooldown: "..time_remaining_str)
             term.setTextColor(colors.white)
             local cur_x, cur_y = term.getCursorPos()
             term.setCursorPos(1, cur_y)
